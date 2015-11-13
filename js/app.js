ed8fcc5529d45e4457c72ae95ac4777a7b9ed681
Matt Katz <mrmattkatz@gmail.com>
2015-11-13 11:51:10 -0600

    Refactor code

--- js/app.js
+++ js/app.js
@@ -1,13 +1,4 @@
 $(document).ready(function() {
-    //API called when search button clicked
-    $('#search').on('click', function(e) {
-        //clears ig-div
-        var imgDiv = $('.ig-div');
-        imgDiv.remove();
-        //clears HTML for each new load
-        $('#instafeed').html("");
-        //search value entered
-        var searchValue = $('#search-input').val();
         //API call
         var feed = new Instafeed({
             get: 'tagged',
@@ -15,5 +6,4 @@ $(document).ready(function() {
             clientId: '4400571e47ce4c69812827874dca620a'
         });
         feed.run();
-    });
 });
